5c26c802df581fa44ed54dd98c79156ed5c967a6
Paolo Anastagi <poliez.poliez.p@gmail.com>
2018-06-24 18:55:33 +0200

    Refactor ToDo main class
    ToDo.java has been refactored to meet code standards for private
    variables and to host some utility methods.

--- prog-java-client/ToDos/src/todos/ToDo.java
+++ prog-java-client/ToDos/src/todos/ToDo.java
@@ -5,7 +5,7 @@
  */
 package todos;
 
-import java.util.*;
+import java.util.Date;
 import javafx.beans.property.*;
 
 /**
@@ -14,66 +14,95 @@ import javafx.beans.property.*;
  */
 public class ToDo
 {
-    SimpleStringProperty incaricato;
-    SimpleStringProperty compito;
-    Date data;
-    SimpleStringProperty ora;
-    SimpleStringProperty descrizione;
+
+    int _id;
+    SimpleStringProperty _incaricato;
+    SimpleStringProperty _compito;
+    Date _data;
+    SimpleStringProperty _ora;
+    SimpleStringProperty _descrizione;
 
     // <editor-fold desc="Getters e Setters">
-    
+    public int getId ()
+    {
+        return _id;
+    }
+
     public SimpleStringProperty getIncaricato ()
     {
-        return incaricato;
+        return _incaricato;
     }
 
     public void setIncaricato (SimpleStringProperty incaricato)
     {
-        this.incaricato = incaricato;
+        this._incaricato = incaricato;
     }
 
     public SimpleStringProperty getCompito ()
     {
-        return compito;
+        return _compito;
     }
 
     public void setCompito (SimpleStringProperty compito)
     {
-        this.compito = compito;
+        this._compito = compito;
     }
 
     public Date getData ()
     {
-        return data;
+        return _data;
     }
 
     public void setData (Date data)
     {
-        this.data = data;
+        this._data = data;
     }
 
     public SimpleStringProperty getOra ()
     {
-        return ora;
+        return _ora;
     }
 
     public void setOra (SimpleStringProperty ora)
     {
-        this.ora = ora;
+        this._ora = ora;
     }
 
     public SimpleStringProperty getDescrizione ()
     {
-        return descrizione;
+        return _descrizione;
     }
 
     public void setDescrizione (SimpleStringProperty descrizione)
     {
-        this.descrizione = descrizione;
+        this._descrizione = descrizione;
     }
-    
+
     // </editor-fold>
-    
-    
-    
+    public String getDataFormattataPerDatabase ()
+    {
+        String[] time = _ora.getValue().split(":");
+        _data.setHours(Integer.parseInt(time[0]));
+        _data.setMinutes(Integer.parseInt(time[1]));
+
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        return sdf.format(_data);
+    }
+
+    public ToDo (int id,
+                 String incaricato,
+                 String compito,
+                 Date data,
+                 String ora,
+                 String descrizione)
+    {
+        _id = id;
+        _incaricato = new SimpleStringProperty(incaricato);
+        _compito = new SimpleStringProperty(compito);
+        _data = data;
+        _ora = new SimpleStringProperty(ora);
+        _descrizione = new SimpleStringProperty(descrizione);
+    }
+
 }
